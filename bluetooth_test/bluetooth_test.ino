9dfc36604c0ab38af6ef5d38548cfded8d614490
KevinJCHe <kevinjc.he@gmail.com>
2019-11-09 14:40:35 -0500

    integrated SoftwareSerial for bluetooth

--- bluetooth_test/bluetooth_test.ino
+++ bluetooth_test/bluetooth_test.ino
@@ -1,12 +1,20 @@
+#include <SoftwareSerial.h>
+
+// bluetooth
+#define RXpin 10 // bluetooth's TX pin goes here
+#define TXpin 11 // bluetooth's RX pin goes here
+SoftwareSerial BTSerial(RXpin, TXpin); // RX | TX
+
 int data;
 int command = 0;
 
 void setup() {
   Serial.begin(9600); 
+  BTSerial.begin(9600); 
 }
 void loop() {
-  if(Serial.available() > 0){ // Checks whether data is comming from the bluetooth serial port
-      data = Serial.read(); // Reads the data from the bluetooth serial port
+  if(BTSerial.available() > 0){ // Checks whether data is comming from the bluetooth serial port
+      data = BTSerial.read(); // Reads the data from the bluetooth serial port
       if((data >= 65 && data <= 72) || (data >= 97 && data <= 104))
         command = data;
   }
